36d8279d368149d1926709a7ead3c4bb0a1bdb94
LawaKiss-cp\LawaKiss <wannisa.kh56@gmail.com>
2017-01-21 20:12:14 +0700

    add publicizes (view)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,12 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
 Route::get('/indexpublicize', 'PublicizeController@indexnew');
 
 Route::get('/template',function(){
