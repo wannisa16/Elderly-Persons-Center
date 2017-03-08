b406b422acd1e4ac80dcb516ff5f383f1433f120
PG69 <jittarinkidrob@gmail.com>
2017-03-08 10:51:20 +0700

    detailelderly css php

--- resources/views/elderly/detailelderly.blade.php
+++ resources/views/elderly/detailelderly.blade.php
@@ -4,7 +4,7 @@
 @endsection
 
 @section('link')
-<link href="{{ URL::asset('elderly/css/home.css') }}" rel="stylesheet" type="text/css" media="all" />
+<link href="{{ URL::asset('elderly/css/detailelderly.css') }}" rel="stylesheet" type="text/css" media="all" />
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 @endsection
 
@@ -12,124 +12,94 @@
 @endsection
 
 @section('content')
+
 <div class="container">
 
-    <div class="row">
-            <label class="layout-title col m12 s12 z-depth-3 "><p class="font-title "><i class="material-icons">perm_identity</i> ข้อมูลทั่วไป</p>
-            </label>
+  <div class="row">
+    <div class="col s12 m12">
+      <div class="card ">
+      <div class="card-image waves-effect waves-block waves-light">
+        <label class="layout-title col m12 s12 z-depth-3 " style="margin-top: auto"><p class="font-title "><i class="material-icons">perm_identity</i> ข้อมูลทั่วไป</p></label>
+        
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4"><p class="font-detail ">รหัส :</p></div>
+                <div class="col m8"><p class="font-detail ">123456</p></div>
+            </div>
+            <div class="col m6"></div>
+        </div>
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4"><p class="font-detail ">ชื่อ :</p></div>
+                <div class="col m5"><p class="font-detail ">นายสมแล้ว</p></div>
+            </div>
+            <div class="col m6">
+                <div class="col m4"><p class="font-detail ">สกุล :</p></div>
+                <div class="col m5"><p class="font-detail ">ที่เป็น</p></div>
+            </div>
+        </div> 
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">เพศ :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">ชาย</p></div>
+            </div>
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">อายุ :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">50</p></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">เกิด :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">12/7/30</p></div>
+            </div>
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">สถาณะ :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">แต่งงาน</p></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m12">
+                <div class="col m2" class="font-detail "><p class="font-detail ">ที่อยู :</p></div>
+                <div class="col m10" class="font-detail "><p class="font-detail ">บ้านเลขที่ 17/18 หมู่ 1 ต.ห้วยนาง อ.ห้วยยอด จ.ตรัง</p></div>
+
+        </div>        
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">อาชีพ :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">รับจ้าง</p></div>
+            </div>
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">ระดับการศึกษา :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">ม4</p></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">บุตร :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">0</p></div>
+            </div>
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">เหตุผล :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">ไม่ทราบ</p></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m6">
+                <div class="col m4" class="font-detail "><p class="font-detail ">flie :</p></div>
+                <div class="col m5" class="font-detail "><p class="font-detail ">0</p></div>
+            </div>
+            <div class="col m6">
+            </div>
+        </div>
+
+        </div>
+      </div>
     </div>
+  </div>
+  </div>
 
-    <div class="row">
-    
-<ul class="collection">
-      
 
-        <div class="col m1 s12 ">
-        <p >รหัส</p>
-        </div>
-        
-        <div class="col m11 s12 ">
-        <p>123456</p>
-        </div>
-        
-        <div class="col m1 s12 ">
-        <p>ชื่อ</p>
-        </div>
-        
-        <div class="col m2 s12 ">
-        <p>นายสมหมาย</p>
-        </div>
-        
-        <div class="col m1 s12 ">
-        <p>สกุล</p>
-        </div>
-        
-        <div class="col m8 s12 ">
-        <p>อยู่นาน</p>
-        </div>
-
-        <div class="col m1 s12 ">
-        <p>วันเกิด</p>
-        </div>
-        
-        <div class="col m2 s12 ">
-        <p>1/2/2520</p>
-        </div>
-        
-        <div class="col m1 s12 ">
-        <p>อายุ</p>
-        </div>
-        <div class="col m8 s12 ">
-        <p>50</p>
-        </div>
-        
-        <div class="col m1 s12 ">
-        <p>เพศ</p>
-        </div>
-        
-        <div class="col m2 s12 ">
-        <p>ชาย</p>
-        </div>
-
-        <div class="col m1 s12 ">
-        <p>สถาณะ</p>
-        </div>
-        
-        <div class="col m8 s12 ">
-        <p>โสด</p>
-        </div>
-        
-        <!-- <div class="col m6 s12 ">
-        <p>child</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ppp</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>อาชีพ</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ppp</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ระดับการศึกษา</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ppp</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>address</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ppp</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>เขต</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ppp</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>เหตุผล</p>
-        </div>
-
-        <div class="col m6 s12 ">
-        <p>ppp</p>
-        </div> -->
-
-        </ul>
-</div>
-</div>
 @endsection
 
 @section('footer')
