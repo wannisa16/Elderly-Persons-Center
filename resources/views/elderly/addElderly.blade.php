09129cf8b24f5fa2a5c7b3e2a3142d95304b53c1
nartruthai <nartruthai.nkr@gmail.com>
2017-05-12 22:06:36 +0700

    Edit design on  Addelderly.

--- resources/views/elderly/addElderly.blade.php
+++ resources/views/elderly/addElderly.blade.php
@@ -17,35 +17,43 @@
 			<div class="card card-define col m12">
 				<div class="row">
         			<div class="input-field col m5 offset-m1 s12">
-          				<input id="name" type="text" class="validate">
+          				<input id="name" type="text" class="validate" name="">
           				<label for="name">ชื่อ :</label>
         			</div>
         			<div class="input-field col m5 s12">
-          				<input id="surname" type="text" class="validate">
+          				<input id="surname" type="text" class="validate" name="">
           				<label for="surname">นามสกุล :</label>
         			</div>
         		</div>
-        		<div class="row">
-        			<div class="col m5 offset-m1 s12">
-          				<input type="date" id="myDate">
-        			</div>
-        			<div class="file-field input-field col m5 s12">
-      					<div class="btn">
-        					<span>แนบเอกสาร</span>
-        					<input type="file" class="file"> 
-      					</div>
-      					<div class="file-path-wrapper">
-        					<input class="file-path validate" type="text" placeholder="แนบเอกสาร">
-      					</div>
-    				</div>
-        		</div>
+
+                <div class="row">
+                    <div class="col m2 offset-m1 s12">
+                        <h5>วัน/เดือน/ปีเกิด</h5>
+                    </div>
+                    <div class="col m8 s12">
+                        <input type="date" id="myDate" name="">
+                    </div>                   
+                </div>
+        		
+                <div class="row">
+                    <div class="file-field input-field col m10 offset-m1 s12">
+                        <div class="btn">
+                            <span>แนบเอกสาร</span>
+                            <input type="file" class="file"> 
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="แนบเอกสาร">
+                        </div>
+                    </div>                   
+                </div>
+
         		<div class="row">
                     <div class="col m2 offset-m1 s12">
                         <h5>เพศ</h5>
                     </div>
                     <div class="col m3 s12">
                         <p>
-                            <input class="with-gap" name="sex" type="radio" id="male"  />
+                            <input class="with-gap" name="sex" type="radio" id="male" />
                             <label for="male">ชาย</label>
                         </p>
                     </div>
@@ -58,11 +66,11 @@
                 </div>
       			<div class="row">
         			<div class="input-field col m5 offset-m1 s12">
-          				<input id="first_name" type="text" class="validate">
+          				<input id="first_name" type="text" class="validate" name="">
           				<label for="first_name">การศึกษา :</label>
         			</div>
         			<div class="input-field col m5 s12">
-          				<input id="first_name" type="text" class="validate">
+          				<input id="first_name" type="text" class="validate" name="">
           				<label for="first_name">อาชีพ :</label>
         			</div>
         		</div>
@@ -112,17 +120,17 @@
                 </div>
                 <div class="row">
         			<div class="input-field col m5 offset-m1 s12">
-          				<input id="first_name" type="number" class="validate">
+          				<input id="first_name" type="number" class="validate" name="">
           				<label for="first_name">บุตร :</label>
         			</div>
         			<div class="input-field col m5 s12">
-          				<input id="first_name" type="text" class="validate">
+          				<input id="first_name" type="text" class="validate" name="">
           				<label for="first_name">จังหวัดที่อยู่ปัจจุบัน :</label>
         			</div>
         		</div>
       			<div class="row">
         			<div class="input-field col m10 offset-m1 s12">
-          				<textarea id="textarea1" class="materialize-textarea"></textarea>
+          				<textarea id="textarea1" class="materialize-textarea" name=""></textarea>
           				<label for="textarea1">รายละเอียดเพิ่มเติม :</label>
         			</div>
       			</div>
@@ -137,25 +145,25 @@
                     <div class="col m11 ">
                         <div class="col m3 offset-m1 s12">
                             <div class="input-field">
-                                <input  name="number" type="text" class="validate">
+                                <input  name="number" type="text" class="validate" name="">
                                 <label>บ้านเลขที่ :</label>
                             </div>
                         </div>
                         <div class="col m2 s12">
                             <div class="input-field">
-                                <input name="village" type="number" class="validate">
+                                <input name="village" type="number" class="validate" name="">
                                 <label >หมู่ที่ :</label>
                             </div>
                         </div>
                         <div class="col m3 s12">
                             <div class="input-field">
-                                <input name="alley" type="text" class="validate">
+                                <input name="alley" type="text" class="validate" name="">
                                 <label>ตรอก :</label>
                             </div>
                         </div>
                         <div class="col m3 s12">
                             <div class="input-field">
-                                <input name="lane" type="text" class="validate">
+                                <input name="lane" type="text" class="validate" name="">
                                 <label>ซอย :</label>
                             </div>
                         </div>                          
@@ -166,19 +174,19 @@
                     <div class="col m11 s12">
                         <div class="col m4 offset-m1 s12">
                             <div class="input-field">
-                                <input name="road" type="text" class="validate">
+                                <input name="road" type="text" class="validate" name="">
                                 <label>ถนน :</label>
                             </div>
                         </div>
                         <div class="col m4 s12">
                             <div class="input-field">
-                                <input name="subDistrict" type="text" class="validate">
+                                <input name="subDistrict" type="text" class="validate" name="">
                                 <label>ตำบล/แขวง :</label>
                             </div>
                         </div>
                         <div class="col m3 s12">
                             <div class="input-field">
-                                <input name="district" type="text" class="validate">
+                                <input name="district" type="text" class="validate" name="">
                                 <label>อำเภอ/เขต :</label>
                             </div>
                         </div>           
@@ -189,13 +197,13 @@
                     <div class="col m11 s12">
                         <div class="col m5 offset-m1 s12">
                             <div class="input-field">
-                                <input name="province" type="text" class="validate">
+                                <input name="province" type="text" class="validate" name="">
                                 <label>จังหวัด :</label>
                             </div>
                         </div>
                         <div class="col m6 s12">
                             <div class="input-field">
-                                <input name="postal" type="number" class="validate">
+                                <input name="postal" type="number" class="validate" name="">
                                 <label>รหัสไปรษณีย์ :</label>
                             </div>
                         </div>                    
