db089d497c2d226492c6b9ae60b8d1bee483f1c4
nartruthai <nartruthai.nkr@gmail.com>
2017-05-12 21:27:08 +0700

    Edit design Addelderly.

--- public/elderly/js/addElderly.js
+++ public/elderly/js/addElderly.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function() {
     Materialize.updateTextFields();
-  });
-$(document).ready(function() {
-  $('.datepicker').pickadate({
+ });
+
+$(document).ready(function(){
+	$('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+    });
 });
